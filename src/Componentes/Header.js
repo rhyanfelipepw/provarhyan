9d431ca3e6ea3a56c68a2bfd354e76060edda253
Marcelo Fortes <marcelofortes306@gmail.com>
2020-11-16 23:06:46 -0300

    finalizei
    teste

--- src/Componentes/Header.js
+++ src/Componentes/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import '../Style/Header.css';
 import { Link } from 'react-router-dom';
+import logo from '../Img/teste.png'
 
 export default function Header() {
   return ( 
     <>
     <div className='body1'>
         <div className="header">
+        <img src={logo} width="150px" />
            <ul>
                <Link to="/">
                 <li>       
